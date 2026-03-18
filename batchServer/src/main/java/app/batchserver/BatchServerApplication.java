574b7b5d8d76840883bc1a7e7ec9f3774bf8c894
水谷 圭佑/MIZUTANI <mizutanikeisuke_ccs@jp.nttdata.com>
2026-03-19 03:12:25 +0900

    最初のコミット

---
+++ batchServer/src/main/java/app/batchserver/BatchServerApplication.java
@@ -0,0 +1,14 @@
+package app.batchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchServerApplication.class, args);
+		System.out.println("きたきた");
+	}
+
+}
